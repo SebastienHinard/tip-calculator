67037f44259cce3c07e177d66c3fecbdd27b47dc
Sebastien HINARD <sebastienhinard@mbpdesebastien.home>
2021-07-18 00:42:50 +0200

    logique dropdown dans l'application vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,13 +13,38 @@ const app = new Vue({
         people: null,
     },
     methods: {
-        toggleLangDropdown: function (e, close = false) {
+        toggleLangDropdown: function () {
             const select_lang = document.querySelector('.select-lang')
-            if (select_lang.classList.contains('open') ||
-                (close && select_lang !== e.target && !select_lang.contains(e.target))) {
+            if (select_lang.classList.contains('open')) {
                 select_lang.classList.remove('open')
             } else {
                 select_lang.classList.add('open')
+            }
+        },
+        toggleAttribution: function () {
+            const attribution_toggle = document.querySelector('.attribution__toggle')
+            const attribution_content = document.querySelector('.attribution')
+
+            if (attribution_toggle.classList.contains('active')) {
+                attribution_toggle.classList.remove('active')
+                attribution_content.classList.remove('active')
+            } else {
+                attribution_toggle.classList.add('active')
+                attribution_content.classList.add('active')
+            }
+        },
+        closeDropdowns: function (e) {
+            const select_lang = document.querySelector('.select-lang')
+            if (select_lang !== e.target && !select_lang.contains(e.target)) {
+                select_lang.classList.remove('open')
+            }
+
+            const attribution_toggle = document.querySelector('.attribution__toggle')
+            const attribution_content = document.querySelector('.attribution')
+            if (e.target !== attribution_content && !attribution_content.contains(e.target) &&
+                e.target !== attribution_toggle && !attribution_toggle.contains(e.target) ) {
+                attribution_toggle.classList.remove('active')
+                attribution_content.classList.remove('active')
             }
         },
         setLang : function (_lang) {
@@ -56,27 +81,4 @@ const app = new Vue({
             return this.currency + (Number(this.bill) / 100 * _tip / Number(this.people) + Number(this.bill) / Number(this.people)).toFixed(2)
         },
     }
-})
-
-// Attribution toggle
-const attribution_toggle = document.querySelector('.attribution__toggle')
-const attribution_content = document.querySelector('.attribution')
-attribution_toggle.addEventListener('click', function (e) {
-    e.preventDefault()
-
-    if (attribution_toggle.classList.contains('active')) {
-        attribution_toggle.classList.remove('active')
-        attribution_content.classList.remove('active')
-    } else {
-        attribution_toggle.classList.add('active')
-        attribution_content.classList.add('active')
-    }
-})
-
-document.body.addEventListener('click', function (e) {
-    if (e.target === attribution_content || attribution_content.contains(e.target) ||
-        e.target === attribution_toggle || attribution_toggle.contains(e.target) ) return false
-
-    attribution_toggle.classList.remove('active')
-    attribution_content.classList.remove('active')
 })
