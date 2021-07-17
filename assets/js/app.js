361b2a991d8bafa893554397dce32a2aa489031b
Sebastien HINARD <sebastienhinard@mbpdesebastien.home>
2021-07-17 19:09:28 +0200

    translation system added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,61 @@
+const translations = {
+    en: {
+        bill: 'Bill',
+        how_much_is_your_bill: 'How much is your bill?',
+        your_bill: 'Your bill',
+        select_tip: 'Select tip %',
+        must_be_numeric: 'Must be numeric',
+        cant_be_zero: 'Can\'t be zero',
+        cant_be_negative: 'Can\'t be negative',
+        add_tip: 'Add a {{1}}% tip',
+        add_custom_tip: 'Add a custom Tip',
+        custom: 'Custom',
+        number_of_people: 'Number of people',
+        how_many_people: 'How many people ?',
+        how_many_people_are_you: 'How many people are you ?',
+        can_you_divide_people: 'Can you divide people ?',
+        tip_amount: 'Tip amount',
+        total: 'Total',
+        per_person: '/ person',
+        reset: 'Reset',
+        reset_informations: 'Reset informations',
+        click_to_open_attribute: 'Click to open informations',
+        challenge_by: 'Challenge by',
+        coded_by: 'Coded by'
+    },
+    fr: {
+        bill: 'Facture',
+        how_much_is_your_bill: 'Montant de la facture?',
+        your_bill: 'Votre facture',
+        select_tip: 'Choisir le pourboire %',
+        must_be_numeric: 'Doit être un nombre',
+        cant_be_zero: 'Ne peut pas être zéro',
+        cant_be_negative: 'Ne peut pas être négatif',
+        add_tip: 'Ajouter un pourboire de {{1}}%',
+        add_custom_tip: 'Ajouter un pourboire personnalisé',
+        custom: 'Autre',
+        number_of_people: 'Nombre de personnes',
+        how_many_people: 'Combien êtes-vous ?',
+        how_many_people_are_you: 'Combien de personnes êtes-vous ?',
+        can_you_divide_people: 'Vous pouvez divisez les gens ?',
+        tip_amount: 'Montant du pourboire',
+        total: 'Total',
+        per_person: '/ personne',
+        reset: 'Remise à zéro',
+        reset_informations: 'Reset informations',
+        click_to_open_attribute: 'Cliquez pour ouvrir les informations',
+        challenge_by: 'Challenge par',
+        coded_by: 'Programmé par'
+    }
+}
+
+
 // Tip calculator app
 const app = new Vue({
     el: '#calculator_app',
     data: {
+        lang: 'fr',
+        translations: translations,
         currency: '$',
         static_tips: [5, 10, 15, 25, 50],
         tip: null,
@@ -16,6 +70,14 @@ const app = new Vue({
         },
         reset: function () {
             this.tip = this.custom_tip = this.bill = this.people = null
+        },
+        translate: function (text, values = []) {
+            let str = translations[this.lang][text] || ''
+            for (let n = 0; n < values.length; n ++) {
+                str = str.replace('{{' + (n + 1) + '}}', values[n])
+            }
+
+            return str
         }
     },
     computed: {
@@ -30,7 +92,7 @@ const app = new Vue({
 
             const _tip = Number(this.custom_tip ? this.custom_tip : this.tip)
             return this.currency + (Number(this.bill) / 100 * _tip / Number(this.people) + Number(this.bill) / Number(this.people)).toFixed(2)
-        }
+        },
     }
 })
 
